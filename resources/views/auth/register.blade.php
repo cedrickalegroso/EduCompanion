749758ed256c477119a0469647b624fa0465333a
Cedrick Alegroso <42290274+itsdancedrick@users.noreply.github.com>
2019-08-21 12:29:50 +0800

    12w345678
    wdqwwqadasds

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -16,11 +16,11 @@
 
 
                                 <div class="col-md-6">
-                                  <input type="text" placeholder="Enter Fullname" name="name" class="form-control" />
+                                  <input type="text" placeholder="Enter fullname" name="name" class="form-control" />
                                 </div>
 
                                 <div class="col-md-6">
-                                    <input type="text" placeholder="Enter Username" name="username" class="form-control" />
+                                    <input type="text" placeholder="Enter username" name="username" class="form-control" />
                                        </div>
                                         </div>
                                      </div>
@@ -48,7 +48,20 @@
                                                 <option value="Central Phillippine University">Central Phillippine University</option>
                                                 <option value="University of San Agustin">University of San Agustin</option>
                                                 <option value="West Visayas State University">West Visayas State University</option>
-                                                 <option value="University of Iloilo">University of Iloilo</option>
+                                                 <option value="Ateneo De Iloilo">Ateneo De Iloilo</option>
+                                                 <option value="St. Joseph School">St. Joseph School</option>
+                                                 <option value="Iloilo National High School">Iloilo National High School</option>
+                                                 <option value="Assumption Iloilo">Assumption Iloilo</option>
+                                                 <option value="Western Institute of Technology">Western Institute of Technology</option>
+                                                 <option value="Iloilo Scholastic Academy">Ateneo De Iloilo</option>
+                                                 <option value="Santa Clarita">Santa Clarita</option>
+                                                 <option value="Hua Siong College of Iloilo">Hua Siong College of Iloilo</option>
+                                                 <option value="Iloilo Montessori">Iloilo Montessori</option>
+                                                 <option value="St. Paul University Iloilo">St. Paul University Iloilo</option>
+                                                 <option value="John B. Lacson Foundation Maritime University">John B. Lacson Foundation Maritime University</option>
+                                                 <option value="STI College Iloilo">STI College Iloilo</option>
+                                                 <option value="Iloilo Doctors College">Iloilo Doctors College</option>
+                                                 <option value="Sacred Heart De Iloilo">Sacred Heart De Iloilo</option>
                                                  </select>
                                         
                                                 <br>
