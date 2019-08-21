22110303da0d37388ea8519f7254e7b5b0774b80
Cedrick Alegroso <42290274+itsdancedrick@users.noreply.github.com>
2019-08-21 10:37:28 +0800

    kio

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -13,14 +13,52 @@
 
                         <div class="container">
                             <div class="row">
+
+
                                 <div class="col-md-6">
                                   <input type="text" placeholder="Enter Fullname" name="name" class="form-control" />
                                 </div>
+
                                 <div class="col-md-6">
-                                    <input type="text" placeholder="Enter username" name="username" class="form-control" />
+                                    <input type="text" placeholder="Enter Username" name="username" class="form-control" />
+                                       </div>
+                                        </div>
+                                     </div>
+                                  <br>
+
+
+                                  <div class="container">
+                            <div class="row">
+
+
+                                <div class="col-md-6">
+                                  <input type="email" placeholder="Enter email" name="email" class="form-control" />
                                 </div>
-                            </div>
-                        </div>
+
+                                <div class="col-md-6">
+                                    <input type="password" placeholder="Enter password" name="password" class="form-control" />
+                                       </div>
+                                        </div>
+                                     </div>
+                                  <br>
+                                      <div class="container">
+                                        <div class="row form">
+                                            <div class="col-md-12">
+                                                 <select class= form-control>
+                                                <option value="Central Phillippine University">Central Phillippine University</option>
+                                                <option value="University of San Agustin">University of San Agustin</option>
+                                                <option value="West Visayas State University">West Visayas State University</option>
+                                                 <option value="University of Iloilo">University of Iloilo</option>
+                                                 </select>
+                                        
+                                                <br>
+
+                                            <button class="btn btn-lg btn-success form-control" > Register </button>
+                                            </div>
+                                        </div>
+                                      </div>
+                                      
+
                     </form>
                 </div>
             </div>
