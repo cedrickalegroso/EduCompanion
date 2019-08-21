45977f371d2a5f81e0d43e7d63e90c15ed93f8d2
Dancedrick <personal.dancedrick@gmail.com>
2019-08-21 11:03:44 +0800

    patch 1.6

---
+++ resources/views/user/quizgames/mathquizgame.blade.php
@@ -0,0 +1,64 @@
+@extends('layouts.user')
+
+@section('content')
+
+<br />
+
+<h3 class="text-info"> Math Quizzes </h3>
+<hr>
+
+<br />
+
+
+@php
+    
+
+    $rand1 = rand(1,999);
+    $rand2 = rand(1,999);
+
+    $randomarithmetic = array('+', '-', '*', '/');
+    $randomize = $randomarithmetic[rand(0,3)]
+
+    switch ($randomize) {
+    case "+":
+        $answer = $rand1 + $rand1;
+        $points = 1;
+        break;
+    case "-":
+        $answer = $rand1 - $rand1;
+        $points = 2;
+        break;
+    case "*":
+        $answer = $rand1 * $rand1;
+        $points = 3;
+        break;
+    case "/":
+        $answer = $rand1 / $rand1;
+        $points = 4;
+        break;
+}
+
+
+  
+
+
+@endphp
+
+
+<div class="container">
+
+
+    <h1> {{ $rand1 }} {{$randomize}} {{  $rand2 }} </h1>
+
+
+    <form action="">
+        <input type="text" class="form-control" name="useranswer" />
+
+        <button class="btn btn-md btn-success">
+             Submit
+        </button>
+    </form>
+
+  
+</div>
+@endsection
